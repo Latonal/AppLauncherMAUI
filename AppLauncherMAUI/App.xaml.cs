10f719492e0865a4404c186cb3ce305b942c90cb
Latonal <reflexion85@gmail.com>
2024-12-14 22:12:04 +0100

    theme picker and preferences saved

--- AppLauncherMAUI/App.xaml.cs
+++ AppLauncherMAUI/App.xaml.cs
@@ -6,7 +6,20 @@
         {
             InitializeComponent();
 
+            SetTheme();
+
             MainPage = new AppShell();
+        }
+
+        private void SetTheme()
+        {
+            int savedTheme = Preferences.Get("AppTheme", 0);
+            UserAppTheme = savedTheme switch
+            {
+                1 => AppTheme.Light,
+                2 => AppTheme.Dark,
+                _ => AppTheme.Unspecified,
+            };
         }
     }
 }
--- AppLauncherMAUI/MVVM/Views/SettingsView.xaml.cs
+++ AppLauncherMAUI/MVVM/Views/SettingsView.xaml.cs
@@ -2,8 +2,47 @@ namespace AppLauncherMAUI.MVVM.Views;
 
 public partial class SettingsView : ContentPage
 {
-	public SettingsView()
-	{
-		InitializeComponent();
-	}
+    public SettingsView()
+    {
+        InitializeComponent();
+
+        ThemePicker.SelectedIndex = GetCurrentThemeIndex();
+    }
+
+    private void OnThemePickerChanged(object sender, EventArgs e)
+    {
+        if (ThemePicker.SelectedIndex == -1)
+            return;
+
+        switch (ThemePicker.SelectedIndex)
+        {
+            case 1:
+                if (Application.Current is not null)
+                    Application.Current.UserAppTheme = AppTheme.Light;
+                break;
+            case 2:
+                if (Application.Current is not null)
+                    Application.Current.UserAppTheme = AppTheme.Dark;
+                break;
+            case 0:
+            default:
+                if (Application.Current is not null)
+                    Application.Current.UserAppTheme = AppTheme.Unspecified;
+                break;
+        }
+
+        Preferences.Set("AppTheme", ThemePicker.SelectedIndex);
+    }
+
+    private static int GetCurrentThemeIndex()
+    {
+        AppTheme currentTheme = Application.Current is not null ? Application.Current.RequestedTheme : AppTheme.Unspecified;
+
+        return currentTheme switch
+        {
+            AppTheme.Light => 1,
+            AppTheme.Dark => 2,
+            _ => 0, // Default - System
+        };
+    }
 }
