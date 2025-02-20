5e17df13ee295b87c4c96bd93483d03d97528a30
Latonal <reflexion85@gmail.com>
2025-02-20 23:56:13 +0100

    banner aspect ratio function

--- AppLauncherMAUI/MVVM/Views/SingleAppView.xaml.cs
+++ AppLauncherMAUI/MVVM/Views/SingleAppView.xaml.cs
@@ -4,10 +4,23 @@ namespace AppLauncherMAUI.MVVM.Views;
 
 public partial class SingleAppView : ContentView
 {
+	private const double TargetAspectRatio = 35 / 9;
+
     public SingleAppView(int appId)
 	{
 		InitializeComponent();
 
 		BindingContext = new SingleAppViewModel(appId);
+	}
+
+	private void OnBannerSizeChanged(object sender, EventArgs e)
+	{
+		if (sender is Border border)
+		{
+			double width = border.Width;
+			double height = width / TargetAspectRatio;
+
+			border.HeightRequest = height;
+		}
 	}
 }
