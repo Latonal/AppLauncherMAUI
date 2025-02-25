752b6d9dd718f236fdf115c9f708195625a2deac
Latonal <reflexion85@gmail.com>
2025-02-25 22:31:37 +0100

    more localization tools

---
+++ AppLauncherMAUI/Utilities/Common.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace AppLauncherMAUI.Utilities;
+
+internal static class Common
+{
+    public static string GetUserLanguage(bool useLong = false)
+    {
+        CultureInfo currentCulture = CultureInfo.CurrentCulture;
+        return useLong ? currentCulture.Name : currentCulture.TwoLetterISOLanguageName;
+    }
+}
