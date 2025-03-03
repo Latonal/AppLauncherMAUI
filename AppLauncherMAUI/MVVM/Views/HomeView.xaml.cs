fff1d5d162177664966a23d72fc7bb302fe29e7e
Latonal <reflexion85@gmail.com>
2025-03-03 01:59:55 +0100

    changed how to display the appcards

--- AppLauncherMAUI/MVVM/Views/HomeView.xaml.cs
+++ AppLauncherMAUI/MVVM/Views/HomeView.xaml.cs
@@ -10,4 +10,18 @@ public partial class HomeView : ContentView
 
         BindingContext = new HomeViewModel();
     }
+
+    public void OnCollectionViewSizeChanged(object sender, EventArgs e)
+    {
+        if (sender is CollectionView cv)
+        {
+            double maxWidth = cv.Width;
+
+            if (cv.ItemsLayout is GridItemsLayout grid)
+            {
+                // size of card + margin = 220 + 20
+                grid.Span = (int)Math.Floor(maxWidth / 240);
+            }
+        }
+    }
 }
